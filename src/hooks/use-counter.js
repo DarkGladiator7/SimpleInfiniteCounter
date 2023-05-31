17afeb485d9d042a42d31f967871b5646b352d38
DarkGladiator7 <ganeshkrishnadoss@gmail.com>
2023-05-31 10:49:05 +0530

    added custom hook

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -1,1 +1,16 @@
-const use
+import { useState,useEffect } from "react";
+const useCounter = ()=>{
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setCounter((prevCounter) => prevCounter + 1);
+      }, 1000);
+  
+      return () => clearInterval(interval);
+    }, []);
+    
+    return counter;
+};
+
+export default useCounter;   
